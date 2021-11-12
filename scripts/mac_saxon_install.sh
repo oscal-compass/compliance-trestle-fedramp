760c266143673a6aad370342e03f65d2dc977b39
Chris Butler <chris@thebutlers.me>
2021-11-12 12:23:29 +1100

    fix: Fix paths.
    Signed-off-by: Chris Butler <chris@thebutlers.me>

--- scripts/mac_saxon_install.sh
+++ scripts/mac_saxon_install.sh
@@ -20,9 +20,9 @@ echo 'See .github/workflows/python-test.yml'
 # Altered for  mac os unzip
 wget -O /tmp/saxon.zip https://www.saxonica.com/saxon-c/libsaxon-HEC-mac-setup-v1.2.1.zip
 unzip /tmp/saxon.zip -d /tmp/saxon
+
+
 sudo mv /tmp/saxon /opt/saxonica
-sudo ln -s /opt/saxonica/libsaxonhec.dylib /usr/lib/libsaxonhec.dylib \
-sudo ln -s /opt/saxonica/rt /usr/lib/rt
 
 echo 'Building saxon python bindings'
 cd /opt/saxonica/Saxon.C.API/python-saxon \
