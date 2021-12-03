403de39b9d349c3793b64635f33bc65bdc95dfe9
semantic-release <semantic-release>
2021-12-03 06:55:23 +0000

    0.2.0
    Automatically generated by python-semantic-release

--- trestle_fedramp/__init__.py
+++ trestle_fedramp/__init__.py
@@ -15,4 +15,4 @@
 # limitations under the License.
 """trestle_fedramp an extension to compliance trestle for functionality specific to the FedRAMP program."""
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
