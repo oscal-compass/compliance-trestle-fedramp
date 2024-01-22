8b3cbb7fcc358a48e9434a5fdbce2124781552e9
Jennifer Power <barnabei.jennifer@gmail.com>
2024-01-22 15:14:50 -0500

    feat: updates project to use saxonc-he version 12.4.1
    Official python wheel packages are available for
    saxonc home edition. This updates the code and setup config to
    use that version.
    
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>

--- tests/trestle_fedramp/core/fedramp_test.py
+++ tests/trestle_fedramp/core/fedramp_test.py
@@ -56,14 +56,14 @@ def test_validate_ssp(tmp_path: pathlib.Path) -> None:
     with open(file_path, 'r') as f:
         json_content = f.read()
     validator = FedrampValidator()
-    success = validator.validate_ssp(json_content, 'json')
+    success = validator.validate_ssp(json_content, 'json', tmp_path)
     assert not success
 
     file_path = pathlib.Path(test_utils.XML_FEDRAMP_SSP_PATH) / test_utils.XML_FEDRAMP_SSP_NAME
     with open(file_path, 'r') as f:
         xml_content = f.read()
     validator = FedrampValidator()
-    success = validator.validate_ssp(xml_content, 'xml')
+    success = validator.validate_ssp(xml_content, 'xml', tmp_path)
     assert not success
 
 
